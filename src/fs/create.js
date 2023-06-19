d4c9bed25bc00bff2dfbb200caa5997c7fa5e1cb
Agnia Staravoitava <agnia2000@inbox.ru>
2023-06-19 22:16:03 +0100

    feat: finish create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
     // Write your code here 
+  const folder = path.dirname(fileURLToPath(import.meta.url))
+  const file = path.join(folder, 'files', 'fresh.txt');
+
+  if (fs.existsSync(file)) {
+    throw new Error('File creation operation failed: File already exists.');
+  }
+
+  try {
+    fs.writeFileSync(file, 'I am fresh and young');
+  } catch (error) {
+    throw new Error(`File creation operation failed: ${error.message}`);
+  }
 };
 
 await create();
