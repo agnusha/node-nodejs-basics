f941adedc0fdd3225836f6fdc5bb546c3e7c9749
Agnia Staravoitava <agnia2000@inbox.ru>
2023-06-19 23:35:13 +0100

    refactor: use async code

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,21 +1,29 @@
-import path from 'path';
-import fs from 'fs';
+import path from 'node:path';
+import { access, writeFile, constants } from 'node:fs/promises';
 import { fileURLToPath } from 'url';
 
 const create = async () => {
     // Write your code here 
-  const folder = path.dirname(fileURLToPath(import.meta.url))
-  const file = path.join(folder, 'files', 'fresh.txt');
+    const folder = path.dirname(fileURLToPath(import.meta.url))
+    const file = path.join(folder, 'files', 'fresh.txt');
 
-  if (fs.existsSync(file)) {
-    throw new Error('File creation operation failed: File already exists.');
-  }
-
-  try {
-    fs.writeFileSync(file, 'I am fresh and young');
-  } catch (error) {
-    throw new Error(`File creation operation failed: ${error.message}`);
-  }
+    try {
+        if (await fileExists(file)) {
+            throw new Error('FS operation failed');
+        }
+        await writeFile(file, 'I am fresh and young');
+    } catch (error) {
+        throw new Error(`File creation operation failed: ${error.message}`);
+    }
 };
+
+async function fileExists(file) {
+    try {
+        await access(file);
+        return true;
+    } catch {
+        return false;
+    }
+}
 
 await create();
