ffd5dc7d3a6c47dcf0d5187068523d76a5a39daf
Agnia Staravoitava <agnia2000@inbox.ru>
2023-06-20 20:26:57 +0100

    feat: finish fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { dirname, join } from 'node:path';
+import { isExists } from './helper.js';
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
     // Write your code here 
+    const folder = dirname(fileURLToPath(import.meta.url))
+    const file = join(folder, 'files', 'fileToRead.txt');
+
+    try {
+        if (!await isExists(file)) {
+            throw new Error('FS operation failed');
+        }
+        const fileContent = await readFile(file, 'utf-8');
+        console.log(fileContent);
+
+    } catch (error) {
+        console.error(`File reading operation failed: ${error.message}`);
+    }
+
 };
 
 await read();
