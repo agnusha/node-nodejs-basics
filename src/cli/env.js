8e9413b2ed142f54aff35b7e630b7cd0e696d3f8
Agnia Staravoitava <agnia2000@inbox.ru>
2023-06-20 21:27:45 +0100

    feat: finish cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
     // Write your code here 
+    const vars = Object.entries(process.env)
+        .filter(([name]) => name.startsWith('RSS_'))
+        .map(([name, value]) => `${name}=${value}`)
+        .join('; ');
+
+    console.log(vars);
 };
 
 parseEnv();
