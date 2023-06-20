984599b747756c97325b9edff0a347c438ba0e9d
Agnia Staravoitava <agnia2000@inbox.ru>
2023-06-20 08:57:20 +0100

    feat: create copy.js basic logic

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import { dirname, join } from 'node:path';
+import { cp } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+
 const copy = async () => {
     // Write your code here 
+
+    const folder = dirname(fileURLToPath(import.meta.url))
+    const fileSource = join(folder, 'files');
+    const fileDestination = join(folder, 'files_copy');
+
+    try {
+        await cp(fileSource, fileDestination, { recursive: true })
+
+    } catch (error) {
+        console.error(`File copy operation failed: ${error.message}`);
+    }
+
 };
 
 await copy();
