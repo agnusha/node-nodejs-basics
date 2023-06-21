af22ba1c1fb9697e0f5db5eb5eb80d034db88a6c
Agnia Staravoitava <agnia2000@inbox.ru>
2023-06-21 11:46:01 +0100

    feat: finish hash logic

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { createHash } from 'crypto';
+import { dirname, join } from 'node:path';
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+
 const calculateHash = async () => {
     // Write your code here 
+    const file = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToCalculateHashFor.txt');
+    const fileData = await readFile(file);
+
+    const hash = createHash('sha256').update(fileData).digest('hex')
+    console.log(hash);
 };
 
 await calculateHash();
