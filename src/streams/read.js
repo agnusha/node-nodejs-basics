8d1311cabd625eef2376968d59def7fc983b3da2
Agnia Staravoitava <agnia2000@inbox.ru>
2023-06-21 16:09:22 +0100

    refactor: use async at streams read and write

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,12 +2,13 @@ import { dirname, join } from 'node:path';
 import { stdout } from 'node:process';
 import { createReadStream } from 'node:fs';
 import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
 
 const read = async () => {
     const file = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToRead.txt')
 
     const readableStream = createReadStream(file, 'utf8');
-    readableStream.pipe(stdout);
+    await pipeline(readableStream, stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -2,13 +2,14 @@ import { dirname, join } from 'node:path';
 import { stdin } from 'node:process';
 import { createWriteStream } from 'node:fs';
 import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
 
 const write = async () => {
     // Write your code here 
     const file = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToWrite.txt')
 
     const writableStream = createWriteStream(file, 'utf8');
-    stdin.pipe(writableStream);
+    await pipeline(stdin, writableStream);
 };
 
 await write();
