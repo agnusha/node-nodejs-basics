7359b2445d8ae5ae11b84b84420a95b5a353f32c
Agnia Staravoitava <agnia2000@inbox.ru>
2023-06-21 15:33:17 +0100

    feat: finish working with stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { dirname, join } from 'node:path';
+import { stdin } from 'node:process';
+import { createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
 const write = async () => {
     // Write your code here 
+    const file = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToWrite.txt')
+
+    const writableStream = createWriteStream(file, 'utf8');
+    stdin.pipe(writableStream);
 };
 
 await write();
