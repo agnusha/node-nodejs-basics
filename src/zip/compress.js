ff5111a939e33c039e6209ec0b1959fa95883037
Agnia Staravoitava <agnia2000@inbox.ru>
2023-06-21 16:02:29 +0100

    feat: implement compress logic

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
 const compress = async () => {
     // Write your code here 
+
+    const folder = join(dirname(fileURLToPath(import.meta.url)), 'files');
+    const fileSource = join(folder, 'fileToCompress.txt');
+    const fileDestination = join(folder, 'archive.gz');
+
+    const source = createReadStream(fileSource);
+    const destination = createWriteStream(fileDestination);
+    const gunzip = createGzip();
+
+    await pipeline(source, gunzip, destination);
 };
 
 await compress();
